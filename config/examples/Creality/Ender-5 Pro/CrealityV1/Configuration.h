f8b5e3b7b59ea2a3905e33b6c919f643156882c9
Chris Watson <710406+c24w@users.noreply.github.com>
2022-07-24 19:15:37 +0100

    Slow down homing a bit
    160 absolutely smashes the endstops 😬
    
    The Z was probably a big aggressive too.

--- config/examples/Creality/Ender-5 Pro/CrealityV1/Configuration.h
+++ config/examples/Creality/Ender-5 Pro/CrealityV1/Configuration.h
@@ -1883,7 +1883,7 @@
 #endif
 
 // Homing speeds (linear=mm/min, rotational=°/min)
-#define HOMING_FEEDRATE_MM_M { (160*60), (160*60), (10*60) }
+#define HOMING_FEEDRATE_MM_M { (100*60), (100*60), (7*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
